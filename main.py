5a678636d744d6e04f475e4f8916f8f9626e0dbc
ADEE328 <dee.ag@obmontessori.edu.ph>
2025-09-08 19:23:48 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+
+restaurant_name = "Manila Eats" # String
+owner_name = "Aaron Dee" # String
+year_established = 1990 # Integer
+popular_item_price = 120 # Integer
+has_delivery = True # Boolean
+product_names = ["Fried chicken", "Carbonara", "Burger"] # List
+business_hours = "Mon-Sun: 10:00 AM - 10:00 PM" # String
+menu_prices = {
+    "Fried Chicken w/ Rice": 120, # Integer
+    "Seafood Pasta": 160, # Integer
+    "Iceream": 110, # Integer
+    "Drinks (Iced tea, softdrinks, shakes)": 20, # Integer
+    "Pizza": 50 # Integer
+}
+common_allergens = ["Seafood", "Dairy", "Gluten"] # List
+tax_rate = 0.10 # Float
